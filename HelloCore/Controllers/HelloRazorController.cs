e25ee6232c9c9d5389ad3c5b7335b8d38b60c08c
Ronny Van Beek <r0849413@student.thomasmore.be>
2021-09-22 14:58:02 +0200

    Add project files.

---
+++ HelloCore/Controllers/HelloRazorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloCore.Controllers
+{
+    public class HelloRazorController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Hoi Razor";
+            return View();
+        }
+
+        public IActionResult Hallo(string naam, int aantal)
+        {
+            ViewData["Message"] = "Hallo " + naam;
+            ViewData["Aantal"] = aantal;
+            return View();
+        }
+    }
+}
---
+++ HelloCore/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloCore.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloCore/
+
+        public string Index()
+        {
+            return "Dit is de 'index' action method";
+        }
+
+        // GET: /HelloCore/Welkom/
+
+        public string Welkom()
+        {
+            return "Dit is de welkom Action Method";
+        }
+
+        public string Bestelling(int id)
+        {
+            return "Dit zijn de details van bestelling met id " + id;
+        }
+
+        public string Boodschap(string voornaam, string boodschap)
+        {
+            return "Boodschap van " + voornaam + ": " + boodschap;
+        }
+    }
+}
